9bb4b23686db183ad6fbc9bfb2cabb8acbfdb61a
computer-note <planomenes@gmail.com>
2024-07-27 21:35:09 +0900

    bugfix: vite.config.js 경로별칭 설정 오류 수정
    jsconfig.json의 compilerOptions.paths의 내용과
    vite.config.js의 resolve.alias의 내용을 맞춰줍니다.
    
    (vite.config.js)
    find: '@',
    replacement: path.resolve(__dirname, 'src')
    
    (jsconfig.json)
    "@/*": ["./src/*"]

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     alias: [
       {
         find: '@',
-        replacement: path.resolve(__dirname),
+        replacement: path.resolve(__dirname, 'src'),
       },
     ],
   },
